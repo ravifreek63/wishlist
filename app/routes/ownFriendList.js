98e336b5da431841a08ead2d27f3e85cbf8194e7
John Stogin <jstogin@gmail.com>
2014-03-18 15:42:13 -0400

    Added initial/incomplete support for a friend list.

---
+++ app/routes/ownFriendList.js
@@ -0,0 +1,35 @@
+/* This file contains handlers for the owner's friend list (both viewing and editing). */
+
+/* The (temporary) view */
+exports.friend_list_view = function (req, res){
+    var userId = req.params.userId;
+    res.render('friend_list', { userId: userId });
+};
+
+/* Return all of the friends for a user. */
+exports.getFriends = function (req, res){
+    // TODO: This is just temporary code.
+    res.writeHead(200, {'content-type': 'text/json' });
+    res.write( JSON.stringify({friends: ["Adam","Benny","Carl"]}) );
+    res.end('\n');
+};
+
+/* add a friend */
+exports.addFriend = function (req, res) {
+    // TODO: Needs implementation.
+};
+
+/* remove a friend */
+exports.removeFriend = function (req, res) {
+    // TODO: Needs implementation.
+};
+
+/* invite a friend */
+exports.inviteFriend = function (req, res) {
+    // TODO: Needs implementation.
+};
+
+/* edit a friend */
+exports.editFriend = function (req, res) {
+    // TODO: Needs implementation.
+};
