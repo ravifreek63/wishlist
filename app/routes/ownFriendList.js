fd55e48e09da67b5028642e4d8423a3c2192b314
John Stogin <jstogin@gmail.com>
2014-03-19 21:04:08 -0400

    The friend list now interfaces with the database.

--- app/routes/ownFriendList.js
+++ app/routes/ownFriendList.js
@@ -1,5 +1,7 @@
 /* This file contains handlers for the owner's friend list (both viewing and editing). */
 
+var methods = require('../helperMethods/methods.js');
+
 /* The (temporary) view */
 exports.friend_list_view = function (req, res){
     var userId = req.params.userId;
@@ -8,24 +10,33 @@ exports.friend_list_view = function (req, res){
 
 /* Return all of the friends for a user. */
 exports.getFriends = function (req, res){
-    // TODO: This is just temporary code.
-    res.writeHead(200, {'content-type': 'text/json' });
-    res.write( JSON.stringify({friends: ["Adam","Benny","Carl"]}) );
-    res.end('\n');
+    var userId = req.params.userId;
+    var query = "SELECT * FROM Group_Relationships WHERE UserId = '"+userId+"';";
+    console.log("In function getFriends, query:"+query);
+    var resMsg = "Get Friends query successful";
+    var resMsgErr = "Get Friends query failed";
+    methods.runQuery(resMsg, resMsgErr, query, res);
 };
 
 /* add a friend */
 exports.addFriend = function (req, res) {
-    // TODO: Needs implementation.
+    var userId = req.params.userId;
+    var name = req.body.friendName;
+    var resMsg = "added friend successfully";
+    var resMsgErr = "Error adding friend";
+    var query = "INSERT INTO Group_Relationships (UserId, Name) Values ('"+userId+"','"+name+"');";
+    methods.runQuery(resMsg, resMsgErr, query, res);
 };
 
 /* remove a friend */
 exports.removeFriend = function (req, res) {
     var userId = req.params.userId;
-    var friendToRemove = req.body.removeFriendWithName;
-    res.writeHead(200, {'content-type': 'text/json' });
-    res.write( JSON.stringify({status: "removed"} ));
-    res.end('\n');
+    var friendToRemove = req.body.name;
+    var relationshipId = req.body.id;
+    var resMsg = "removed friend successfully";
+    var resMsgErr = "Error removing friend";
+    var query = "DELETE FROM Group_Relationships WHERE Id = "+relationshipId+";";
+    methods.runQuery(resMsg, resMsgErr, query, res);
 };
 
 /* invite a friend */
